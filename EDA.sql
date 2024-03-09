a27d7ef266e560dd6ebd8f99d8192a97a3ac0ff3
Jason Shan <jason.shan55@gmail.com>
2024-03-08 21:16:32 -0500

    Add files via upload

--- EDA.sql
+++ EDA.sql
@@ -3,27 +3,32 @@ Supermarket Sales Data Exploration
 
 
 */
+-- Previewing Data
 
 SELECT *
 FROM supermarket_sales;
 
 
 -- Which products are the best sellers?
-SELECT `Product line`, ROUND(SUM(`Gross income`), 2) AS TotalSales
+
+SELECT `Product line`, COUNT(*) AS SalesCount, ROUND(SUM(`Gross income`), 2) AS TotalSales
 FROM supermarket_sales
 GROUP BY `Product line`
 ORDER BY TotalSales DESC;
 
--- Which City generated most revenue?
+-- Which Branch/City generated most revenue?
 
-SELECT 
-    `City`, ROUND(SUM(`Gross income`), 2) AS TotalSales
-FROM
-    supermarket_sales
-GROUP BY `City`
+SELECT Branch, City, ROUND(SUM(`Gross income`), 2) AS TotalSales
+FROM supermarket_sales
+GROUP BY Branch, City
 ORDER BY TotalSales DESC;
 
 -- What are the peak time periods
+
+SELECT `Time of day`, COUNT(*) AS SalesCount, ROUND(SUM(`Gross income`), 2) AS TotalSales
+FROM supermarket_sales
+GROUP BY `Time of day`
+ORDER BY SalesCount DESC;
 
 
 -- Which month generated the most sales? Jan-Mar
@@ -33,6 +38,14 @@ FROM supermarket_sales;
 
 
 -- Male vs Female spendings
+
+SELECT Gender, COUNT(*) AS SalesCount, ROUND(SUM(`Gross income`), 2) AS TotalSales
+FROM supermarket_sales
+GROUP BY Gender
+ORDER BY TotalSales DESC;
+
+
+
 
 
 
